e243f2fdeeb53c79862d2a44cc91c587c4eaaac9
hankliu <hankliu@augmentum.com.cn>
2016-07-27 12:10:13 +0800

    fullpage: add lint source file

--- fullpage/gulp/tasks/symlink.js
+++ fullpage/gulp/tasks/symlink.js
@@ -4,5 +4,5 @@ var config = require('../config').symlink;
 
 gulp.task('symlink:pre-commit', function() {
   gulp.src(config.src)
-    .pipe(symlink(config.dest, { foce: true }));
+    .pipe(symlink(config.dest, { force: true }));
 })
